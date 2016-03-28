34de21469e6f1c7abcda424f746ea95f0061400f
Flávio Vieira <fvieira@tempotelecom.com.br>
2016-03-28 14:20:57 -0300

    Alterations for publishing to hex.pm

--- mix.exs
+++ mix.exs
@@ -7,25 +7,32 @@ defmodule Phonex.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
+     source_url: "https://github.com/fcevado/phone",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  defp package do
+    [
+      files: ["lib", "mix.exs", "test", "README.md"],
+      maintainers: ["Flávio Moreira Vieira"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/fcevado/phone"}
+    ]
+  end
+
+  defp description do
+    """
+    Parser for phone numbers in international standard.
+    NOT READY FOR USE.
+    """
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
