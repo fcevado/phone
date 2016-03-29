1b92002c0843220b8933e5ce66f3643a6b943ff0
Flávio <flavio.mv@gmail.com>
2016-03-28 23:25:28 -0300

    quebec

---
+++ lib/phone/nanp/ca/qc.ex
@@ -0,0 +1,38 @@
+defmodule Phone.NANP.CA.QC do
+  @regex ~r/^(418|438|450|514|579|581|819|873)([2-9].+)/
+  @province "Quebec"
+  @abbreviation "QC"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 10
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, code, number]] = Regex.scan(@regex,number)
+
+    %{
+      country: Phone.NANP.CA.country,
+      a2: Phone.NANP.CA.a2,
+      a3: Phone.NANP.CA.a3,
+      code: 1,
+      area_code: code,
+      province: @province,
+      province_abbreviation: @abbreviation
+    }
+  end
+
+  def province do
+    @province
+  end
+
+  def abbreviation do
+    @abbreviation
+  end
+end
