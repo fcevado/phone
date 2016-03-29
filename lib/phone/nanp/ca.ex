5e5e21beadcb4cc0a1c9aeb3312db7b5f34ea443
Flávio <flavio.mv@gmail.com>
2016-03-28 22:46:31 -0300

    preparing ca.ex and alberta

--- lib/phone/nanp/ca.ex
+++ lib/phone/nanp/ca.ex
@@ -1,7 +1,24 @@
 defmodule Phone.NANP.CA do
+  @country "Canada"
+  @a2 "CA"
+  @a3 "CAN"
+
   def match?(number) do
   end
 
-  def match(number) do
+  def country do
+    @country
+  end
+
+  def a2 do
+    @a2
+  end
+
+  def a3 do
+    @a3
+  end
+
+  def abbreviation do
+    %{a2: @a2, a3: @a3}
   end
 end
