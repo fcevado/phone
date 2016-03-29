9e6277e191c2ba2e817963ef1b69efbd47584edc
Flávio <flavio.mv@gmail.com>
2016-03-28 23:37:58 -0300

    finalizing canadian area codes

--- lib/phone/nanp/ca.ex
+++ lib/phone/nanp/ca.ex
@@ -2,8 +2,29 @@ defmodule Phone.NANP.CA do
   @country "Canada"
   @a2 "CA"
   @a3 "CAN"
+  @modules [
+    Phone.NANP.CA.AB,
+    Phone.NANP.CA.BC,
+    Phone.NANP.CA.MB,
+    Phone.NANP.CA.NB,
+    Phone.NANP.CA.NL,
+    Phone.NANP.CA.NS_PE,
+    Phone.NANP.CA.ON,
+    Phone.NANP.CA.QC,
+    Phone.NANP.CA.SK,
+    Phone.NANP.CA.Territory
+  ]
 
   def match?(number) do
+    module = Enum.filter(@modules, fn(x) -> x.match?(number) end)
+
+    length(module) > 0
+  end
+
+  def build(number) do
+    [module] = Enum.filter(@modules, fn(x) -> x.match?(number) end)
+
+    module.build(number)
   end
 
   def country do
