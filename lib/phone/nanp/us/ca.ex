b2b9a11408264adcf426de136389c2c6005e3a8a
Flávio Vieira <fvieira@tempotelecom.com.br>
2016-03-30 13:22:46 -0300

    California

---
+++ lib/phone/nanp/us/ca.ex
@@ -0,0 +1,38 @@
+defmodule Phone.NANP.US.CA do
+  @regex ~r/^(209|213|310|323|408|415|510|530|559|562|619|626|650|661|707|714|760|805|818|831|858|909|916|925|949)([2-9].+)/
+  @province "California"
+  @abbreviation "CA"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 10
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, code, number]] = Regex.scan(@regex,number)
+
+    %{
+      country: Phone.NANP.US.country,
+      a2: Phone.NANP.US.a2,
+      a3: Phone.NANP.US.a3,
+      code: 1,
+      area_code: code,
+      province: @province,
+      province_abbreviation: @abbreviation
+    }
+  end
+
+  def province do
+    @province
+  end
+
+  def abbreviation do
+    @abbreviation
+  end
+end
