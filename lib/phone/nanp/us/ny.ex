6e8aaec64ffce2df5f5567bd1cdb3bc96f8b9d68
Flávio <flavio.mv@gmail.com>
2016-03-30 23:08:26 -0300

    New York

---
+++ lib/phone/nanp/us/ny.ex
@@ -0,0 +1,38 @@
+defmodule Phone.NANP.US.NY do
+  @regex ~r/^(212|315|347|516|585|607|631|646|716|718|845|914|917|929)([2-9].+)/
+  @state "New York"
+  @abbreviation "NY"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 10
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, code, number]] = Regex.scan(@regex,number)
+
+    %{
+      country: Phone.NANP.US.country,
+      a2: Phone.NANP.US.a2,
+      a3: Phone.NANP.US.a3,
+      code: 1,
+      area_code: code,
+      state: @state,
+      state_abbreviation: @abbreviation
+    }
+  end
+
+  def state do
+    @state
+  end
+
+  def abbreviation do
+    @abbreviation
+  end
+end
