50f1ac5ad7360807b840e433f58c901b61fad2e8
Flávio <flavio.mv@gmail.com>
2016-03-30 22:07:57 -0300

    Maryland

---
+++ lib/phone/nanp/us/md.ex
@@ -0,0 +1,38 @@
+defmodule Phone.NANP.US.MD do
+  @regex ~r/^(240|301|410|443|667)([2-9].+)/
+  @state "Maryland"
+  @abbreviation "MD"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 10
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, code, number]] = Regex.scan(@regex,number)
+
+    %{
+      country: Phone.NANP.US.country,
+      a2: Phone.NANP.US.a2,
+      a3: Phone.NANP.US.a3,
+      code: 1,
+      area_code: code,
+      state: @state,
+      state_abbreviation: @abbreviation
+    }
+  end
+
+  def state do
+    @state
+  end
+
+  def abbreviation do
+    @abbreviation
+  end
+end
