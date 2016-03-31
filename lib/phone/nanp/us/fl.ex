4a36ed4085b8345c606e98ad683408e957a18517
Flávio <flavio.mv@gmail.com>
2016-03-30 21:31:36 -0300

    Florida

---
+++ lib/phone/nanp/us/fl.ex
@@ -0,0 +1,38 @@
+defmodule Phone.NANP.US.FL do
+  @regex ~r/^(239|305|321|352|386|407|561|727|754|772|786|813|850|863|904|941|954)([2-9].+)/
+  @state "Florida"
+  @abbreviation "FL"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 10
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, code, number]] = Regex.scan(@regex,number)
+
+    %{
+      country: Phone.NANP.US.country,
+      a2: Phone.NANP.US.a2,
+      a3: Phone.NANP.US.a3,
+      code: 1,
+      area_code: code,
+      state: @state,
+      state_abbreviation: @abbreviation
+    }
+  end
+
+  def state do
+    @state
+  end
+
+  def abbreviation do
+    @abbreviation
+  end
+end
