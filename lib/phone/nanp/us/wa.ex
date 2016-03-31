0b065446aacdf29313ea1da462ebdc4094047f76
Flávio <flavio.mv@gmail.com>
2016-03-30 23:41:54 -0300

    Washington

---
+++ lib/phone/nanp/us/wa.ex
@@ -0,0 +1,38 @@
+defmodule Phone.NANP.US.WA do
+  @regex ~r/^(206|253|360|425|509)([2-9].+)/
+  @state "Washington"
+  @abbreviation "WA"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 10
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, code, number]] = Regex.scan(@regex,number)
+
+    %{
+      country: Phone.NANP.US.country,
+      a2: Phone.NANP.US.a2,
+      a3: Phone.NANP.US.a3,
+      code: 1,
+      area_code: code,
+      state: @state,
+      state_abbreviation: @abbreviation
+    }
+  end
+
+  def state do
+    @state
+  end
+
+  def abbreviation do
+    @abbreviation
+  end
+end
