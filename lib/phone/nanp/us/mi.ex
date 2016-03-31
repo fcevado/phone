1cf03f1d021931102981b5b28c42dbaee1495a62
Flávio <flavio.mv@gmail.com>
2016-03-30 22:17:55 -0300

    Michigan

---
+++ lib/phone/nanp/us/mi.ex
@@ -0,0 +1,38 @@
+defmodule Phone.NANP.US.MI do
+  @regex ~r/^(231|248|269|313|517|586|616|734|810|906|947|989)([2-9].+)/
+  @state "Michigan"
+  @abbreviation "MI"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 10
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, code, number]] = Regex.scan(@regex,number)
+
+    %{
+      country: Phone.NANP.US.country,
+      a2: Phone.NANP.US.a2,
+      a3: Phone.NANP.US.a3,
+      code: 1,
+      area_code: code,
+      state: @state,
+      state_abbreviation: @abbreviation
+    }
+  end
+
+  def state do
+    @state
+  end
+
+  def abbreviation do
+    @abbreviation
+  end
+end
