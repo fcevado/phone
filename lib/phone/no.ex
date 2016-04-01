683bcb2a02e6ee00497c289f71766d75db884cf5
Flávio <flavio.mv@gmail.com>
2016-04-01 00:48:20 -0300

    Kingdom of Norway

--- lib/phone/no.ex
+++ lib/phone/no.ex
@@ -1,5 +1,45 @@
-# same code as SJ and BV
 defmodule Phone.NO do
-  def match(number) do
+  @regex ~r/^47([2-8].......|0....)/
+  @country "Norway"
+  @a2 "NO"
+  @a3 "NOR"
+
+  def match?(number) do
+    Regex.match?(@regex, number)
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, number]] = Regex.scan(@regex, number)
+
+    %{
+      code: "47",
+      number: number,
+      country: @country,
+      a2: @a2,
+      a3: @a3
+    }
+  end
+
+  def country do
+    @country
+  end
+
+  def a2 do
+    @a2
+  end
+
+  def a3 do
+    @a3
+  end
+
+  def abbreviation do
+    %{a2: @a2, a3: @a3}
   end
 end
