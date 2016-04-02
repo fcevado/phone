19586eabda221fd3308a4a5fbdc91ba6c96f393b
Flávio <flavio.mv@gmail.com>
2016-04-02 19:50:53 -0300

    Austria

--- lib/phone/at.ex
+++ lib/phone/at.ex
@@ -1,4 +1,45 @@
 defmodule Phone.AT do
-  def match(number) do
+  @regex ~r/^43(.+)/
+  @country "Austria"
+  @a2 "AT"
+  @a3 "AUT"
+
+  def match?(number) do
+    Regex.match?(@regex,number)
+  end
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, number]] = Regex.scan(@regex, number)
+
+    %{
+      code: "93",
+      number: number,
+      country: @country,
+      a2: @a2,
+      a3: @a3
+    }
+  end
+
+  def country do
+    @country
+  end
+
+  def a2 do
+    @a2
+  end
+
+  def a3 do
+    @a3
+  end
+
+  def abbreviation do
+    %{a2: @a2, a3: @a3}
   end
 end
