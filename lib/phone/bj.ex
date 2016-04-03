7da928c5abb6f7a6c6c9a2646872dc6252fe7085
Flávio <flavio.mv@gmail.com>
2016-04-03 15:31:16 -0300

    Benin

--- lib/phone/bj.ex
+++ lib/phone/bj.ex
@@ -1,4 +1,45 @@
 defmodule Phone.BJ do
-  def match(number) do
+  @regex ~r/^229(.+)/
+  @country "Benin"
+  @a2 "BJ"
+  @a3 "BEN"
+
+  def match?(number) do
+    Regex.match?(@regex,number) and String.length(number) == 11
+  end:
+
+  def build(number) do
+    case match?(number) do
+      false -> {:error, "Not a valid number."}
+      true -> {:ok, builder(number)}
+    end
+  end
+
+  def builder(number) do
+    [[_, number]] = Regex.scan(@regex, number)
+
+    %{
+      code: "229",
+      number: number,
+      country: @country,
+      a2: @a2,
+      a3: @a3
+    }
+  end
+
+  def country do
+    @country
+  end
+
+  def a2 do
+    @a2
+  end
+
+  def a3 do
+    @a3
+  end
+
+  def abbreviation do
+    %{a2: @a2, a3: @a3}
   end
 end
