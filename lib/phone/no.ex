d3322fd5fbe2e182f2fc88c3aa9a57cf2e693f8c
Flávio <flavio.mv@gmail.com>
2016-04-13 00:32:27 -0300

    refactoring KZ, NO, RU

--- lib/phone/no.ex
+++ lib/phone/no.ex
@@ -1,45 +1,8 @@
 defmodule Phone.NO do
-  @regex ~r/^47([2-8].......|0....)/
-  @country "Norway"
-  @a2 "NO"
-  @a3 "NOR"
-
-  def match?(number) do
-    Regex.match?(@regex, number)
-  end
-
-  def build(number) do
-    case match?(number) do
-      false -> {:error, "Not a valid number."}
-      true -> {:ok, builder(number)}
-    end
-  end
-
-  def builder(number) do
-    [[_, number]] = Regex.scan(@regex, number)
-
-    %{
-      code: "47",
-      number: number,
-      country: @country,
-      a2: @a2,
-      a3: @a3
-    }
-  end
-
-  def country do
-    @country
-  end
-
-  def a2 do
-    @a2
-  end
-
-  def a3 do
-    @a3
-  end
-
-  def abbreviation do
-    %{a2: @a2, a3: @a3}
-  end
+  use Helper.Country
+  field :regex, ~r/^(47)()([2-8].{7}|0.{3})/
+  field :country, "Norway"
+  field :a2, "NO"
+  field :a3, "NOR"
+  match :regex
 end
