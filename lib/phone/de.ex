49ab5199a5ee13df903b4c54b506eb39b3aa6778
Flávio <flavio.mv@gmail.com>
2016-04-16 17:53:04 -0300

    D Countries

--- lib/phone/de.ex
+++ lib/phone/de.ex
@@ -1,4 +1,8 @@
 defmodule Phone.DE do
-  def match(number) do
-  end
+  use Helper.Country
+  field :regex, ~r/^(49)()(.{10,11})/
+  field :country, "Germany"
+  field :a2, "DE"
+  field :a3, "DEU"
+  match :regex
 end
--- lib/phone/dj.ex
+++ lib/phone/dj.ex
@@ -1,4 +1,8 @@
 defmodule Phone.DJ do
-  def match(number) do
-  end
+  use Helper.Country
+  field :regex, ~r/^(253)()(.{8})/
+  field :country, "Djibouti"
+  field :a2, "DJ"
+  field :a3, "DJI"
+  match :regex
 end
