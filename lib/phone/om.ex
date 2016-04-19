55d3a8e7e563c5afe23268b5ace30be229856262
Flávio Vieira <fvieira@tempotelecom.com.br>
2016-04-19 13:33:49 -0300

    Adding Oman

--- lib/phone/om.ex
+++ lib/phone/om.ex
@@ -1,4 +1,8 @@
 defmodule Phone.OM do
-  def match(number) do
-  end
+  use Helper.Country
+  field :regex, ~r/^(968)()(.{8})/
+  field :country, "Oman"
+  field :a2, "OM"
+  field :a3, "OMN"
+  match :regex
 end
