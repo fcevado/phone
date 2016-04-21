975367fa350afc7c3ecbf3ecc77c861ec0a0317c
Flávio <flavio.mv@gmail.com>
2016-04-21 00:02:34 -0300

    Y Countries

--- lib/phone/ye.ex
+++ lib/phone/ye.ex
@@ -1,4 +1,8 @@
 defmodule Phone.YE do
-  def match(number) do
-  end
+  use Helper.Country
+  field :regex, ~r/^(967)()(.+)/
+  field :country, "Yemen"
+  field :a2, "YE"
+  field :a3, "YEM"
+  match :regex
 end
