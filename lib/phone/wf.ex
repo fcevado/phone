174784abf44b7be9a3512fc657556b95ac7b640b
Flávio <flavio.mv@gmail.com>
2016-04-20 23:54:27 -0300

    W Countries

--- lib/phone/wf.ex
+++ lib/phone/wf.ex
@@ -1,4 +1,8 @@
 defmodule Phone.WF do
-  def match(number) do
-  end
+  use Helper.Country
+  field :regex, ~r/^(681)()(.{6})/
+  field :country, "Wallis and Futuna"
+  field :a2, "WF"
+  field :a3, "WLF"
+  match :regex
 end
--- lib/phone/ws.ex
+++ lib/phone/ws.ex
@@ -1,4 +1,8 @@
 defmodule Phone.WS do
-  def match(number) do
-  end
+  use Helper.Country
+  field :regex, ~r/^(685)()(.{6,7})/
+  field :country, "Samoa"
+  field :a2, "WS"
+  field :a3, "WSM"
+  match :regex
 end
