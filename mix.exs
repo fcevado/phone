3eb96b828fd313e517c5013226b4c088675a70a3
Flávio <flavio.mv@gmail.com>
2016-07-10 15:18:05 -0300

    update minimum elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Phonex.Mixfile do
   def project do
     [app: :phone,
      version: "0.3.0",
-     elixir: ">= 1.0.0",
+     elixir: ">= 1.1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description,
