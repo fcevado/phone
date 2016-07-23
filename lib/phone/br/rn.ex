1564a5e4e1ba957eb32037b41262c1471c3896cb
Flávio <flavio.mv@gmail.com>
2016-07-23 16:25:30 -0300

    Rio Grande do Norte

--- lib/phone/br/rn.ex
+++ lib/phone/br/rn.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.RN do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(84)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Rio Grande do Norte"
   field :area_type, "state"
   field :area_abbreviation, "RN"
