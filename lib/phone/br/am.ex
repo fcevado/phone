621c228017c33ca121d3eb9dd0327a5c0dff8554
Flávio <flavio.mv@gmail.com>
2016-07-23 14:48:57 -0300

    Amazonas

--- lib/phone/br/am.ex
+++ lib/phone/br/am.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.AM do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(9[27])([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Amazonas"
   field :area_type, "state"
   field :area_abbreviation, "AM"
