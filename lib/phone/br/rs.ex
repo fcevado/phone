873d231956f4f27d0fb4d38025ef805573703896
Flávio <flavio.mv@gmail.com>
2016-07-23 16:39:58 -0300

    Rio Grande do Sul

--- lib/phone/br/rs.ex
+++ lib/phone/br/rs.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.RS do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(5[1|3-5])([2-5|89].{7}|9[89].{7}|78.{6})$/
   field :area_name, "Rio Grande do Sul"
   field :area_type, "state"
   field :area_abbreviation, "RS"
