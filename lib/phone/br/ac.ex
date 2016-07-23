13992ef5627e2221367f924833a5a5877a5cde09
Flávio <flavio.mv@gmail.com>
2016-07-23 14:31:42 -0300

    Acre

--- lib/phone/br/ac.ex
+++ lib/phone/br/ac.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.AC do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(68)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Acre"
   field :area_type, "state"
   field :area_abbreviation, "AC"
