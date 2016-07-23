fc829ec6be5f03a1601c3c6e755844e04e47dcdd
Flávio <flavio.mv@gmail.com>
2016-07-23 16:01:51 -0300

    Paraíba

--- lib/phone/br/pb.ex
+++ lib/phone/br/pb.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.PB do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(83)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Paraíba"
   field :area_type, "state"
   field :area_abbreviation, "PB"
