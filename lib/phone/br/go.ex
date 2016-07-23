dc701673f510deeb3637babef9ae479088f22fa6
Flávio <flavio.mv@gmail.com>
2016-07-23 15:07:21 -0300

    Goiás

--- lib/phone/br/go.ex
+++ lib/phone/br/go.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.GO do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(6[24])([2-5|89].{7}|9[89].{7}|78.{6})$/
   field :area_name, "Goiás"
   field :area_type, "state"
   field :area_abbreviation, "GO"
