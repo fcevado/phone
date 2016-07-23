bc8cbba133505016adaf6c959e7021a52dfc00ab
Flávio <flavio.mv@gmail.com>
2016-07-23 16:31:43 -0300

    Rondônia

--- lib/phone/br/ro.ex
+++ lib/phone/br/ro.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.RO do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(69)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Rondônia"
   field :area_type, "state"
   field :area_abbreviation, "RO"
