e53ac806f3b6ae9f55d57ea76f21e3e6280c718d
Flávio <flavio.mv@gmail.com>
2016-07-23 16:08:47 -0300

    Piauí

--- lib/phone/br/pi.ex
+++ lib/phone/br/pi.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.PI do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(8[69])([2-5|89].{7}|9[89].{7})/
   field :area_name, "Piauí"
   field :area_type, "state"
   field :area_abbreviation, "PI"
