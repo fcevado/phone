adffbf742bb8a29db1608a787982f60fd3045e84
Flávio <flavio.mv@gmail.com>
2016-07-23 14:50:03 -0300

    Amapá

--- lib/phone/br/ap.ex
+++ lib/phone/br/ap.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.AP do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(96)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Amapá"
   field :area_type, "state"
   field :area_abbreviation, "AP"
