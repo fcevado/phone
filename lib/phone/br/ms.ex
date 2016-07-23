fdf2837e444b04c4b3ca980b8dae45efaa266da0
Flávio <flavio.mv@gmail.com>
2016-07-23 15:31:42 -0300

    Mato Grosso do Sul

--- lib/phone/br/ms.ex
+++ lib/phone/br/ms.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.MS do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(67)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Mato Grosso do Sul"
   field :area_type, "state"
   field :area_abbreviation, "MS"
