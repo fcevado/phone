9251eeb28f3e46225ac069a81b09e33b1521cd2e
Flávio <flavio.mv@gmail.com>
2016-07-23 16:40:10 -0300

    Santa Catarina

--- lib/phone/br/sc.ex
+++ lib/phone/br/sc.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.SC do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(4[7-9])([2-5|89].{7}|9[89].{7}|78.{6})$/
   field :area_name, "Santa Catarina"
   field :area_type, "state"
   field :area_abbreviation, "SC"
