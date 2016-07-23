caf7575514584036d1588913721d7f8f65bfa728
Flávio <flavio.mv@gmail.com>
2016-07-23 16:55:26 -0300

    Sergipe

--- lib/phone/br/se.ex
+++ lib/phone/br/se.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.SE do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(79)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Sergipe"
   field :area_type, "state"
   field :area_abbreviation, "SE"
