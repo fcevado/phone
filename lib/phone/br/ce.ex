1af2e924a4dabe68bcf4aa863bf929e5d047041d
Flávio <flavio.mv@gmail.com>
2016-07-23 15:06:29 -0300

    Ceará

--- lib/phone/br/ce.ex
+++ lib/phone/br/ce.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.CE do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(8[58])([2-5|89].{7}|9[89].{7}|78.{6})$/
   field :area_name, "Ceará"
   field :area_type, "state"
   field :area_abbreviation, "CE"
