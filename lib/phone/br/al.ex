727194b29a6f1ab264104893c8120ed663430268
Flávio <flavio.mv@gmail.com>
2016-07-23 14:48:44 -0300

    correct Alagoas regex

--- lib/phone/br/al.ex
+++ lib/phone/br/al.ex
@@ -1,6 +1,6 @@
-defmodule Phone.BR.ALdo
+defmodule Phone.BR.AL do
   use Helper.Area
-  field :regex, ~r/^(55)(82)([2-5|89].{7}|9[89].{7})/
+  field :regex, ~r/^(55)(82)([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Alagoas"
   field :area_type, "state"
   field :area_abbreviation, "AL"
