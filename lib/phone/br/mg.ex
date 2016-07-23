82109a39671b3a27839262523ca266e241dfa0c0
Flávio <flavio.mv@gmail.com>
2016-07-23 15:28:45 -0300

    Minas Gerais

--- lib/phone/br/mg.ex
+++ lib/phone/br/mg.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.MG do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(3[1-5|78])([2-5|89].{7}|9[89].{7}|7[78].{6})$/
   field :area_name, "Minas Gerais"
   field :area_type, "state"
   field :area_abbreviation, "MG"
