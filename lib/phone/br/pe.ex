b264b40689c80218a69d9bbe5b1ca77d335a196d
Flávio <flavio.mv@gmail.com>
2016-07-23 16:04:17 -0300

    Pernambuco

--- lib/phone/br/pe.ex
+++ lib/phone/br/pe.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.PE do
   use Helper.Area
-  field :regex, ~r/^(55)()()/
+  field :regex, ~r/^(55)(8[17])([2-5|89].{7}|9[89].{7})$/
   field :area_name, "Pernambuco"
   field :area_type, "state"
   field :area_abbreviation, "PE"
