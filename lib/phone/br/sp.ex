20b80794ad0ce5fbaafcd92afa25aa84e4fc0976
Flávio <flavio.mv@gmail.com>
2016-08-02 20:29:54 -0300

    Correct Sao Paulo number validation. issue #64

--- lib/phone/br/sp.ex
+++ lib/phone/br/sp.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BR.SP do
   use Helper.Area
-  field :regex, ~r/^(55)(1[1-9])([2-5|89].{7}|9[89].{7}|7[07-9].{6})$/
+  field :regex, ~r/^(55)(1[1-9])([2-5|89].{7}|9.{8}|7[07-9].{6})$/
   field :area_name, "São Paulo"
   field :area_type, "state"
   field :area_abbreviation, "SP"
