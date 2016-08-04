5a7801ff00dcf3789b988770ff29b124b7650a7d
Flávio <flavio.mv@gmail.com>
2016-08-03 21:47:10 -0300

    update to v0.3.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Phonex.Mixfile do
 
   def project do
     [app: :phone,
-     version: "0.3.3",
+     version: "0.3.4",
      elixir: ">= 1.1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
