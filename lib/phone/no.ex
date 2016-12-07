6f5ac21329c89041f7a1a38c39a7df877db2e3ee
Mark Nijhof <mark@snowflake.ai>
2016-12-07 02:07:25 +0100

    Norwegian phone numbers can also start with a 9

--- lib/phone/no.ex
+++ lib/phone/no.ex
@@ -1,6 +1,6 @@
 defmodule Phone.NO do
   use Helper.Country
-  field :regex, ~r/^(47)()([2-8].{7}|0.{3})/
+  field :regex, ~r/^(47)()([2-9].{7}|0.{3})/
   field :country, "Norway"
   field :a2, "NO"
   field :a3, "NOR"
