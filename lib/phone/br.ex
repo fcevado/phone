503138c5dc083f8d157e31cdcae805eaad962fe9
Cevado <flavio.mv@github.com>
2017-04-02 19:03:39 -0300

    update Brazil to new behavior

--- lib/phone/br.ex
+++ lib/phone/br.ex
@@ -1,23 +1,14 @@
 defmodule Phone.BR do
   use Helper.Country
-  field :country, "Brazil"
-  field :a2, "BR"
-  field :a3, "BRA"
-  field :modules, [
-    Phone.BR.AC, Phone.BR.AL,
-    Phone.BR.AM, Phone.BR.AP,
-    Phone.BR.BA, Phone.BR.CE,
-    Phone.BR.DF, Phone.BR.ES,
-    Phone.BR.GO, Phone.BR.MA,
-    Phone.BR.MT, Phone.BR.MS,
-    Phone.BR.MG, Phone.BR.PA,
-    Phone.BR.PB, Phone.BR.PE,
-    Phone.BR.PI, Phone.BR.PR,
-    Phone.BR.RJ, Phone.BR.RN,
-    Phone.BR.RO, Phone.BR.RR,
-    Phone.BR.RS, Phone.BR.SC,
-    Phone.BR.SE, Phone.BR.SP,
-    Phone.BR.TO
-  ]
-  match :modules
+
+  def country, do: "Brazil"
+  def a2, do: "BR"
+  def a3, do: "BRA"
+
+  matcher :modules, [Phone.BR.AC, Phone.BR.AL, Phone.BR.AM, Phone.BR.AP, Phone.BR.BA,
+                     Phone.BR.CE, Phone.BR.DF, Phone.BR.ES, Phone.BR.GO, Phone.BR.MA,
+                     Phone.BR.MT, Phone.BR.MS, Phone.BR.MG, Phone.BR.PA, Phone.BR.PB,
+                     Phone.BR.PE, Phone.BR.PI, Phone.BR.PR, Phone.BR.RJ, Phone.BR.RN,
+                     Phone.BR.RO, Phone.BR.RR, Phone.BR.RS, Phone.BR.SC, Phone.BR.SE,
+                     Phone.BR.SP, Phone.BR.TO]
 end
