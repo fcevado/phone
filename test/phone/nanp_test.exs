10fa660992a4df4c55987b4b5f85dfcc6c844f2a
Cevado <flavio.mv@gmail.com>
2017-04-23 17:17:13 -0300

    Refactor NANP Tests

--- test/phone/nanp_test.exs
+++ test/phone/nanp_test.exs
@@ -1,20 +1,40 @@
-defmodule Phone.NanpTest do
+defmodule Phone.NANPTest do
   use ExUnit.Case, async: true
 
-  @external_resource area_codes_path = Path.join([__DIR__, "..", "support", "nanp-area-codes.csv"])
+  all_modules = [
+    Phone.NANP.AS, Phone.NANP.AI, Phone.NANP.AG, Phone.NANP.BS,
+    Phone.NANP.BB, Phone.NANP.BM, Phone.NANP.CA, Phone.NANP.DM,
+    Phone.NANP.DO, Phone.NANP.GD, Phone.NANP.GU, Phone.NANP.JM,
+    Phone.NANP.KN, Phone.NANP.KY, Phone.NANP.LC, Phone.NANP.MP,
+    Phone.NANP.MS, Phone.NANP.PR, Phone.NANP.SX, Phone.NANP.TC,
+    Phone.NANP.TT, Phone.NANP.US, Phone.NANP.VC, Phone.NANP.VG,
+    Phone.NANP.VI, Phone.NANP.TollFree
+  ]
 
-  for line <- File.stream!(area_codes_path, [], :line) do
-    [area_code, state_abbreviation] = line |> String.strip |> String.split(",")
+  Enum.map(all_modules,
+    fn module ->
+      Enum.map(module.codes,
+        fn code ->
+          test "#{inspect module} parses area code #{code}" do
+            assert Phone.valid?(unquote("#{code}5551234"))
+            assert {:ok, parsed} = Phone.parse(unquote("#{code}5551234"))
 
-    test "correctly parses NANP area code #{area_code} (#{state_abbreviation})" do
-      assert {:ok, result} = Phone.parse(unquote("1#{area_code}5551234"))
+            assert parsed.country == unquote(module).country
+            assert parsed.a2 == unquote(module).a2
+            assert parsed.a3 == unquote(module).a3
+          end
 
-      case result do
-        %{a2: "US"} ->
-          assert result.area_abbreviation == unquote(state_abbreviation)
-        _ ->
-          true
-      end
-    end
-  end
+          test "#{inspect module} cant parse wrong number with code #{code}" do
+            refute Phone.valid?(unquote("#{code}555123"))
+            assert {:error, _} = Phone.parse(unquote("#{code}555123"))
+
+            refute unquote(module).match?(unquote("#{code}555123"))
+            assert {:error, _} = unquote(module).build(unquote("#{code}555123"))
+
+            assert_raise ArgumentError,
+            "Not a valid phone number.",
+            fn -> unquote(module).build!(unquote("#{code}555123")) end
+          end
+        end)
+    end)
 end
