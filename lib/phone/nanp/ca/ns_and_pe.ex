666cf8eec7f72aac9c5459a6b1486c9c1cc4e117
Cevado <flavio.mv@gmail.com>
2017-04-23 00:27:44 -0300

    Resolve comma issues from credo

--- lib/phone/nanp/ca/ns_and_pe.ex
+++ lib/phone/nanp/ca/ns_and_pe.ex
@@ -4,9 +4,9 @@ defmodule Phone.NANP.CA.NSAndPE do
   use Helper.Area
 
   def regex, do: ~r/^(1)(902|782)([2-9].{6})$/
-  def area_name, do: ["Nova Scotia","Prince Edward Island"]
+  def area_name, do: ["Nova Scotia", "Prince Edward Island"]
   def area_type, do: "provinces"
-  def area_abbreviation, do: ["NS","PE"]
+  def area_abbreviation, do: ["NS", "PE"]
 
   matcher ["1902", "1782"]
 end
--- lib/phone/nanp/ca/territory.ex
+++ lib/phone/nanp/ca/territory.ex
@@ -6,7 +6,7 @@ defmodule Phone.NANP.CA.Territory do
   def regex, do: ~r/^(1)(867)([2-9].{6})$/
   def area_name, do: ["Northwest Territories", "Nunavut", "Yukon"]
   def area_type, do: "territories"
-  def area_abbreviation, do: ["NT","NU","YT"]
+  def area_abbreviation, do: ["NT", "NU", "YT"]
 
   matcher ["1867"]
 end
