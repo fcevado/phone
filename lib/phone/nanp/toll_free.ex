b0a91f6a0314069c6512ad3acff86d2f33a0d971
Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>
2017-06-16 13:57:17 -0500

    Add support for 833 toll free numbers

--- lib/phone/nanp/toll_free.ex
+++ lib/phone/nanp/toll_free.ex
@@ -3,10 +3,10 @@ defmodule Phone.NANP.TollFree do
 
   use Helper.Country
 
-  def regex, do: ~r/^(1)(800|844|855|866|877|888)([2-9].{6})$/
+  def regex, do: ~r/^(1)(800|833|844|855|866|877|888)([2-9].{6})$/
   def country, do: "NANP tool-free"
   def a2, do: ""
   def a3, do: ""
 
-  matcher :regex, ["1800", "1844", "1855", "1866", "1877", "1888"]
+  matcher :regex, ["1800", "1833", "1844", "1855", "1866", "1877", "1888"]
 end
