2e82a5c79b16dd3cb7e99ccd4c5c1cc1b111e997
Flavio <flavio@telnyx.com>
2018-03-24 14:40:09 -0300

    Correct NANP Toll Free number typo

--- lib/phone/nanp/toll_free.ex
+++ lib/phone/nanp/toll_free.ex
@@ -4,7 +4,7 @@ defmodule Phone.NANP.TollFree do
   use Helper.Country
 
   def regex, do: ~r/^(1)(800|833|844|855|866|877|888)([2-9].{6})$/
-  def country, do: "NANP tool-free"
+  def country, do: "NANP toll-free"
   def a2, do: ""
   def a3, do: ""
 
