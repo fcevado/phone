49172e03ef95c9ced617c58aacef9158ae1eeff7
Alexander Gorkunov <alexander.gorkunov@gmail.com>
2018-10-10 16:46:42 +0400

    allow brazil numbers with +55 97XXX XXXX

--- lib/phone/br/rj.ex
+++ lib/phone/br/rj.ex
@@ -3,7 +3,7 @@ defmodule Phone.BR.RJ do
 
   use Helper.Area
 
-  def regex, do: ~r/^(55)(2[124])([2-5|89].{7}|9[89].{7}|7[078].{6})$/
+  def regex, do: ~r/^(55)(2[124])([2-5|89].{7}|9.{8}|7[078].{6})$/
   def area_name, do: "Rio de Janeiro"
   def area_type, do: "state"
   def area_abbreviation, do: "RJ"
