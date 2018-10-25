686998243932a0d37bd40edeffff0a4edbcc2769
Alexander Gorkunov <alexander.gorkunov@gmail.com>
2018-10-25 20:15:04 +0400

    fix validation for pakistan phones

--- lib/phone/pk.ex
+++ lib/phone/pk.ex
@@ -3,7 +3,7 @@ defmodule Phone.PK do
 
   use Helper.Country
 
-  def regex, do: ~r/^(92)()(.+'')/
+  def regex, do: ~r/^(92)()(.+)/
   def country, do: "Pakistan"
   def a2, do: "PK"
   def a3, do: "PAK"
