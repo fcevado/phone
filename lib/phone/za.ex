6de4f1272cc7b80102917f322968b1c55114bc17
chakming <chakming@users.noreply.github.com>
2018-10-31 16:44:14 +0800

    ✨ solve south africa parsing issue
    according to https://en.wikipedia.org/wiki/Telephone_numbers_in_South_Africa, 0 is omitted so only 9 digits after the country code 27

--- lib/phone/za.ex
+++ lib/phone/za.ex
@@ -3,7 +3,7 @@ defmodule Phone.ZA do
 
   use Helper.Country
 
-  def regex, do: ~r/^(27)()(.{10})/
+  def regex, do: ~r/^(27)()(.{9})/
   def country, do: "South Africa"
   def a2, do: "ZA"
   def a3, do: "ZAF"
