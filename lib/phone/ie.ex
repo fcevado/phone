a9b283af8bc40214f44739bdb50293896eaca2bf
Cevado <flavio.mv@gmail.com>
2019-02-21 10:39:47 -0300

    Update australian and irish regex

--- lib/phone/ie.ex
+++ lib/phone/ie.ex
@@ -3,7 +3,7 @@ defmodule Phone.IE do
 
   use Helper.Country
 
-  def regex, do: ~r/^(353)(1|402|800|404|505|90|[2-9][1-9])(.{5,7})$/
+  def regex, do: ~r/^(353)(1|402|800|404|505|90|[2-9][1-9])(.{5,9})$/
   def country, do: "Ireland"
   def a2, do: "IE"
   def a3, do: "IRL"
