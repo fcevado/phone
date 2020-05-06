83108ab1042efe62778c7363f5d02ef888883408
David Kovsky <davidk@telnyx.com>
2020-05-06 13:12:25 -0600

    allows 10 digits for Singapore numbers

--- lib/phone/sg.ex
+++ lib/phone/sg.ex
@@ -2,8 +2,7 @@ defmodule Phone.SG do
   @moduledoc false
 
   use Helper.Country
-
-  def regex, do: ~r/^(65)()(.{8})/
+  def regex, do: ~r/^(65)()(.{10}|.{8})/
   def country, do: "Singapore"
   def a2, do: "SG"
   def a3, do: "SGP"
