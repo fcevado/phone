1b91fca485a1fc6f919484d69fb57dbee345ccf7
Cevado <flavio.mv@gmail.com>
2020-12-29 16:22:43 -0300

    remove duplicate modules for JM

--- lib/phone/nanp/jm.ex
+++ lib/phone/nanp/jm.ex
@@ -1,12 +1,12 @@
-defmodule Phone.NANP.JM1 do
+defmodule Phone.NANP.JM do
   @moduledoc false
 
   use Helper.Country
 
-  def regex, do: ~r/^(1)(876)([2-9].{6})$/
+  def regex, do: ~r/^(1)(658|876)([2-9].{6})$/
   def country, do: "Jamaica"
   def a2, do: "JA"
   def a3, do: "JAM"
 
-  matcher(:regex, ["1876"])
+  matcher(:regex, ["1658", "1876"])
 end
