e4c7a6211aba4d00d05a827e717198891f9b2e7f
Nick Riebeek <nick.riebeek@gmail.com>
2023-09-18 13:44:38 -0400

    Add 839 South Carolina area code

--- lib/phone/nanp/us/sc.ex
+++ lib/phone/nanp/us/sc.ex
@@ -3,10 +3,10 @@ defmodule Phone.NANP.US.SC do
 
   use Helper.Area
 
-  def regex, do: ~r/^(1)(803|843|854|864)([2-9].{6})$/
+  def regex, do: ~r/^(1)(803|839|843|854|864)([2-9].{6})$/
   def area_name, do: "South Carolina"
   def area_type, do: "state"
   def area_abbreviation, do: "SC"
 
-  matcher(["1803", "1843", "1854", "1864"])
+  matcher(["1803", "1839", "1843", "1854", "1864"])
 end
