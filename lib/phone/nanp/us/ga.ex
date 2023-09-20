4fdb12454d63b146d351ad141568ba52ec6f2bd8
Robert Ho <robertho@Roberts-MacBook-Pro.local>
2023-09-20 09:50:50 -0700

    add 943 ATL, georgia area code

--- lib/phone/nanp/us/ga.ex
+++ lib/phone/nanp/us/ga.ex
@@ -3,10 +3,10 @@ defmodule Phone.NANP.US.GA do
 
   use Helper.Area
 
-  def regex, do: ~r/^(1)(229|404|470|478|678|706|762|770|912)([2-9].{6})$/
+  def regex, do: ~r/^(1)(229|404|470|478|678|706|762|770|912|943)([2-9].{6})$/
   def area_name, do: "Georgia"
   def area_type, do: "state"
   def area_abbreviation, do: "GA"
 
-  matcher(["1229", "1404", "1470", "1478", "1678", "1706", "1762", "1770", "1912"])
+  matcher(["1229", "1404", "1470", "1478", "1678", "1706", "1762", "1770", "1912", "1943"])
 end
